58711e87a41c291814eb8dacbc27d0f5e1d263db
Dmakrakis <dmakrakis@live.com>
2022-11-22 17:52:26 +0200

    Second commit

---
+++ portfolio-10/schemas/skill.js
@@ -0,0 +1,19 @@
+export default {
+  name: "skill",
+  title: "Skill",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "Title of skill",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: { hotspot: true },
+    },
+  ],
+};
