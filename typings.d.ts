ec270c2ecfbf5143b9940c446ffb6d54849dbf78
Dmakrakis <dmakrakis@live.com>
2022-11-27 22:33:40 +0200

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -9,6 +9,18 @@ interface Image {
   _type: "image";
   asset: { string; _ref: string; _type: "reference" };
 }
+
+interface PageInfo extends SanityBody {
+  _type: "pageInfo";
+  address: string;
+  backgroundInformation: string;
+  email: string;
+  role: string;
+  heroImage: Image;
+  name: string;
+  phoneNumber: string;
+  profilePic: Image;
+}
 export interface Social extends SanityBody {
   _type: "social";
   title: string;
