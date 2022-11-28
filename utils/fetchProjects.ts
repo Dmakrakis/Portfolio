ca3356afe2391d61d6d9a7745bd5dae66246eac0
Dmakrakis <dmakrakis@live.com>
2022-11-29 01:09:43 +0200

    utils

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -2,7 +2,7 @@ import { Project } from "../typings";
 
 export const fetchSocials = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/apo/getProjects`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
   );
 
   const data = await res.json();
